d25eb8d3c0a11d987c000776d0b805a9912feb4d
KJTL290 <kimjoshua.lopez@evsu.edu.ph>
2025-12-20 19:09:51 +0800

    update

--- add_email_field.sql
+++
@@ -1,5 +0,0 @@
--- Add email field to users table
-ALTER TABLE users ADD COLUMN email VARCHAR(100) UNIQUE;
-
--- Update the existing test user with an email
-UPDATE users SET email='test@example.com' WHERE username='test';
